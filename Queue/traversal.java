5f6df2199e4e5b7e8ed2a9f23b2b265151e9a5be
surekha-degala <surekhadegala22@gmail.com>
2025-06-27 09:43:39 +0530

    Create traversal.java
    Given a deque dq containing integer elements, the task is to traverse the dq and print its elements of it. 
    Note: Print a newline at the end.

---
+++ Queue/traversal.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static void printDeque(ArrayDeque<Integer> deq) {
+        // code here
+        for ( int num : deq) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
