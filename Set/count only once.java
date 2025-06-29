33f49cde469a2d87af35020ae0852ed1ff7b083a
surekha-degala <surekhadegala22@gmail.com>
2025-06-29 16:35:42 +0530

    Create count only once.java
    Count Only Once
    
    You are given an integer array arr[] of integers. You need to find how many elements exist if we count the elements only once. That is repeated elements are counted as occuring once.

---
+++ Set/count only once.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+class Solution {
+  public static int countOnce(int arr[]) {
+       Set s =new HashSet();
+        for (int num : arr) {
+          s.add(num)
+            }
+    return s.size();
+  }
+}
