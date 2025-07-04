84f28ec6247aa175ad7dd2be572e2b4bb2cdb4d9
surekha-degala <surekhadegala22@gmail.com>
2025-07-04 21:27:26 +0530

    Create name .java
    printing name n number of times

---
+++ Recursion/name .java
@@ -0,0 +1,11 @@
+class rec {
+  static void func() {
+if (i >n) return ;
+    System.out.println("SUKU");
+    func(i+!, n)
+  }
+
+  public static void main (String [] args ) {
+  int n = 3;
+    func(1, n);
+  }
