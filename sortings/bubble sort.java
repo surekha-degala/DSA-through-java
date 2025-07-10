284f835b31a3b6415653f77a7c4e6cf79a40cfce
surekha-degala <surekhadegala22@gmail.com>
2025-07-10 23:07:18 +0530

    Create bubble sort.java
    Bubble Sort
    
    
    
    Given an array of integers called nums,sort the array in non-decreasing order using the bubble sort algorithm and return the sorted array.
    
    
    
    A sorted array in non-decreasing order is an array where each element is greater than or equal to all preceding elements in the array.
    
    
    Examples:
    Input: nums = [7, 4, 1, 5, 3]
    
    Output: [1, 3, 4, 5, 7]
    
    Explanation: 1 <= 3 <= 4 <= 5 <= 7.
    
    Thus the array is sorted in non-decreasing order.

---
+++ sortings/bubble sort.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] bubbleSort(int[] nums) {
+        int n = nums.length;
+        for (int i = n-1; i>=1; i--) {
+            for (int j = 0; j <= i-1; j++ ) {
+                if (nums[j] > nums[j+1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+            
+        }
+        return nums;
+    }
+}
