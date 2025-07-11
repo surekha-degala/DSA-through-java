ddcc49246e1a1ea994efb0550e42fab195a2576d
surekha-degala <surekhadegala22@gmail.com>
2025-07-11 23:39:44 +0530

    Update second largest.java

--- Array/second largest.java
+++ Array/second largest.java
@@ -1,14 +1,19 @@
 class Solution {
     public int secondLargestElement(int[] nums) {
+        if (nums == null || nums.length < 2) return -1;
+
         int largest = Integer.MIN_VALUE;
-        int s  = Integer.MIN_VALUE;
-        for(int i =0; i<nums.length;i++) {
-            if (nums[i]> largest) {
+        int second = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > largest) {
+                second = largest;
                 largest = nums[i];
-            } else if (nums[i]> s && s != largest) {
-                s = nums[i];
+            } else if (nums[i] > second && nums[i] != largest) {
+                second = nums[i];
             }
         }
-        return s;
+
+        return (second == Integer.MIN_VALUE) ? -1 : second;
     }
 }
