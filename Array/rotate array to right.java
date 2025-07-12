cbe854a7e000b4198886be8ca2a65d2c9c06eecb
surekha-degala <surekhadegala22@gmail.com>
2025-07-13 00:01:25 +0530

    Update rotate array to right.java

--- Array/rotate array to right.java
+++ Array/rotate array to right.java
@@ -3,6 +3,7 @@
 class Solution {
     public void rotate(int[] arr) {
         int n = arr.length;
+        if (n <= 1) return;
         // code here
         int last = arr[n-1];
         for (int i = n-1; i>0;i--) {
