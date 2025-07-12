58c400308727ef95de8fa2bace4b1de1fba05b5e
surekha-degala <surekhadegala22@gmail.com>
2025-07-12 23:58:35 +0530

    Create rotate array to right.java
    Rotate Array by One
    Difficulty: BasicAccuracy: 69.6%Submissions: 318K+Points: 1Average Time: 20m
    Given an array arr, rotate the array by one position in clockwise direction.
    
    Examples:
    
    Input: arr[] = [1, 2, 3, 4, 5]
    Output: [5, 1, 2, 3, 4]
    Explanation: If we rotate arr by one position in clockwise 5 come to the front and remaining those are shifted to the end.
    Input: arr[] = [9, 8, 7, 6, 4, 2, 1, 3]
    Output: [3, 9, 8, 7, 6, 4, 2, 1]
    Explanation: After rotating clock-wise 3 comes in first position.
    Constraints:

---
+++ Array/rotate array to right.java
@@ -0,0 +1,14 @@
+// // User function Template for Java
+
+class Solution {
+    public void rotate(int[] arr) {
+        int n = arr.length;
+        // code here
+        int last = arr[n-1];
+        for (int i = n-1; i>0;i--) {
+            
+            arr[i] = arr[i -1];
+        }
+        arr[0] = last;
+    }
+}
