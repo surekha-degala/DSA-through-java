6d757b53e4edb3bfc41da60f008eac9e913d015a
surekha-degala <surekhadegala22@gmail.com>
2025-07-16 23:35:48 +0530

    Update nine divisors.java

--- gfg-potd/nine divisors.java
+++ gfg-potd/nine divisors.java
@@ -26,3 +26,63 @@ class Solution {
         return count;
     }
 }
+
+optimized 
+
+    import java.util.*;
+
+class Solution {
+    public static int countNumbers(int n) {
+        int count = 0;
+        List<Integer> primes = sieve((int)Math.sqrt(n) + 1);
+
+        // Case 1: Numbers of the form p^8
+        for (int p : primes) {
+            long pow = (long)Math.pow(p, 8);
+            if (pow <= n) {
+                count++;
+            }
+        }
+
+        // Case 2: Numbers of the form p^2 * q^2
+        int size = primes.size();
+        for (int i = 0; i < size; i++) {
+            long p2 = (long)primes.get(i) * primes.get(i);
+            if (p2 > n) break;
+
+            for (int j = i + 1; j < size; j++) {
+                long q2 = (long)primes.get(j) * primes.get(j);
+                if (p2 * q2 <= n) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    // Sieve of Eratosthenes to get all primes up to √n
+    private static List<Integer> sieve(int limit) {
+        boolean[] isPrime = new boolean[limit + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i <= limit; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= limit; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        List<Integer> primes = new ArrayList<>();
+        for (int i = 2; i <= limit; i++) {
+            if (isPrime[i]) primes.add(i);
+        }
+
+        return primes;
+    }
+}
+
