8574eed22a74fcffd8c70b0848b2b39b4632d15b
surekha-degala <surekhadegala22@gmail.com>
2025-07-17 23:48:30 +0530

    Create creating a linked list.java
    creating linked list

---
+++ Linked-Lists/creating a linked list.java
@@ -0,0 +1,18 @@
+class Node {
+  int data;
+  Node next;
+}
+Node (int data1, int Next1) {
+  this.data1 = data1;
+  this.Next1 = Next;
+}
+Node (int data1) {
+this.data1 = data1;
+this.next = null;
+}
+
+public class LinkedList{
+  public static void main (String [] args) {
+    Node x = new Node(arr[0])
+      }
+}
