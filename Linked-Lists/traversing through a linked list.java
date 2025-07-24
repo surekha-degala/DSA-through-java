6836c8ff116c062067a44063a87dccd198919a37
surekha-degala <surekhadegala22@gmail.com>
2025-07-24 16:39:30 +0530

    Create traversing through a linked list.java

---
+++ Linked-Lists/traversing through a linked list.java
@@ -0,0 +1,24 @@
+Class Node {
+  int data;
+  Node next;
+
+  Node (int data1, Node next1) {
+    this.data1 = data1;
+    this.next = next;
+}
+
+  Node (int data1) {
+    this.data1 = data1;
+    this.next = null;
+  }
+}
+
+public class LinkedList  {
+  public static void main (String[] args ) {
+    Node y = new Node(arr[0]);
+    Node temp = head;
+    while (temp! = null) {
+      System.out.println(temp.data);
+      temp = temp.next;
+  }
+}
