4e619861b9c8c58bc07f4e16aef98372730318ba
surekha-degala <surekhadegala22@gmail.com>
2025-07-26 22:29:21 +0530

    Update deleting head.java

--- Linked-Lists/deleting head.java
+++ Linked-Lists/deleting head.java
@@ -4,3 +4,14 @@ public int void removehead (Node head) {
   head = head.next;
   return head;
 }
+
+
+class Solution {
+    public ListNode deleteHead(ListNode head) {
+        if (head == null) return null;
+        return head.next;
+    }
+}
+
+//sleek one liner
+return head == null ? null : head.next; 
