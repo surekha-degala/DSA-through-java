7d434d596e4305b582bc8af36dd68d1ccee7bdee
surekha-degala <surekhadegala22@gmail.com>
2025-07-27 20:32:01 +0530

    Create Deleting head .java

---
+++ Linked-Lists/doubly linked list/Deleting head .java
@@ -0,0 +1,36 @@
+/*Definition of doubly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode prev;
+
+    ListNode() {
+        val = 0;
+        next = null;
+        prev = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+        prev = null;
+    }
+
+    ListNode(int data1, ListNode next1, ListNode prev1) {
+        val = data1;
+        next = next1;
+        prev = prev1;
+    }
+}
+ */
+
+class Solution {
+    public ListNode deleteHead(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode prev = head;
+        head = head.next;
+        head.prev = null;
+        prev.next = null;
+        return head;
+    }
+}
