7174818bc4549e462263f4f0b03169ad25177d04
surekha-degala <surekhadegala22@gmail.com>
2025-07-27 12:45:00 +0530

    Create converting2arr.java

---
+++ Linked-Lists/doubly linket list/converting2arr.java
@@ -0,0 +1,48 @@
+class Node {
+    int data;
+    Node next, prev;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+
+    Node(int data, Node next, Node prev) {
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
+public class DLL {
+
+    private static void print(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    private static Node convert2dll(int[] arr) {
+        if (arr.length == 0) return null;
+
+        Node head = new Node(arr[0]);
+        Node prev = head;
+
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i], null, prev);
+            prev.next = temp;
+            prev = temp;
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        Node head = convert2dll(arr);
+        print(head);
+    }
+}
