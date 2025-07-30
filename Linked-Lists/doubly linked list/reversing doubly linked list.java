21aec1dd53c31c9ca5737c742e8ef3fd94725347
surekha-degala <surekhadegala22@gmail.com>
2025-07-30 14:40:27 +0530

    Create reversing doubly linked list.java

---
+++ Linked-Lists/doubly linked list/reversing doubly linked list.java
@@ -0,0 +1,30 @@
+/*
+class DLLNode {
+    int data;
+    DLLNode next;
+    DLLNode prev;
+
+    DLLNode(int val) {
+        data = val;
+        next = null;
+        prev = null;
+    }
+}
+*/
+class Solution {
+    public DLLNode reverseDLL(DLLNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // Your code here
+        DLLNode prev = null;
+        DLLNode curr = head;
+        while (curr != null) {
+            prev = curr.prev;
+            curr.prev = curr.next;
+            curr.next = prev;
+            curr = curr.prev;
+        }
+        return prev.prev;
+    }
+}
