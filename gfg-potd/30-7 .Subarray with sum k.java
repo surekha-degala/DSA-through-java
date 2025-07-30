84e0feb3176395f1bfcaf44f374cd13e9f5910c7
surekha-degala <surekhadegala22@gmail.com>
2025-07-30 23:28:57 +0530

    Update 30-7 .Subarray with sum k.java

--- gfg-potd/30-7 .Subarray with sum k.java
+++ gfg-potd/30-7 .Subarray with sum k.java
@@ -16,3 +16,30 @@ class Solution {
         return count;
     }
 }
+//optimized
+
+import java.util.HashMap;
+
+class Solution {
+    public int cntSubarrays(int[] arr, int k) {
+        int subarrayCount = 0;
+        int currentSum = 0;
+
+        HashMap<Integer, Integer> sumFrequency = new HashMap<>();
+        sumFrequency.put(0, 1); // to count subarrays starting from index 0
+
+        for (int num : arr) {
+            currentSum += num;
+
+            int neededSum = currentSum - k;
+
+            if (sumFrequency.containsKey(neededSum)) {
+                subarrayCount += sumFrequency.get(neededSum);
+            }
+
+            sumFrequency.put(currentSum, sumFrequency.getOrDefault(currentSum, 0) + 1);
+        }
+
+        return subarrayCount;
+    }
+}
