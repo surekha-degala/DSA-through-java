5d39512aa7c05017bc722e1c133e7a0c7a9ef4e8
surekha-degala <surekhadegala22@gmail.com>
2025-07-31 18:16:38 +0530

    Update Pow(x,n).java

--- Recursion/Pow(x,n).java
+++ Recursion/Pow(x,n).java
@@ -7,3 +7,23 @@ class Solution {
         return result;
     }
 }
+//optimized code 
+
+class Solution {
+    public double myPow(double x, int n) {
+     if (n ==0 ) return 1;
+    if (n<0) {
+        if (n == Integer.MIN_VALUE) {
+                return 1 / (x * myPow(x, -(n + 1)));
+            }
+        return 1 / myPow(x, -n);
+    }
+
+    double half = myPow(x, n/2);
+        if (n%2 == 0 ) {
+            return half * half;
+        } else {
+            return x * half * half;
+        }
+    }
+    }
