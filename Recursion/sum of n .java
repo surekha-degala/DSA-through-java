0f73980114569feae40b2fdb2eded2762a568318
surekha-degala <surekhadegala22@gmail.com>
2025-08-01 11:52:11 +0530

    Update sum of n .java

--- Recursion/sum of n .java
+++ Recursion/sum of n .java
@@ -12,3 +12,20 @@ class Solution {
         System.out.println(obj.NnumbersSum(N));
     }
 }
+
+//
+
+
+class Main {
+    public static int sumi(int n ) {
+        if (n == 1) {
+            return 1;
+        }
+        int sum = n + sumi(n-1);
+        return sum;
+    }
+    public static void main(String[] args) {
+    int result = sumi(4);
+    System.out.println(result);
+        }
+}
