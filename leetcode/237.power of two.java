d0676594b247bb9e0dcda04bd119a30572127f03
surekha-degala <surekhadegala22@gmail.com>
2025-08-09 22:59:56 +0530

    Update 237.power of two.java

--- leetcode/237.power of two.java
+++ leetcode/237.power of two.java
@@ -9,4 +9,34 @@ class Solution {
         }
         return false;
     }
+}// This will exceed time complexity.
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        for (int i = 0; i<=Math.sqrt(n)+1; i++) {
+            if (Math.pow(2, i)== n) {
+                return true;
+            }
+        }
+        return false;
+    }
+}// this will be bruteforce and take 6 ms.
+
+//here goes the optimal solution 
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+     if (n <= 0) return false;
+        while (n %2 ==0) {
+            n/=2;
+        }
+        return n == 1;
+    }
+}
+//here goes the bit method
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if ( n <= 0) return false;
+        return (n & n-1) ==0;
+    }
 }
