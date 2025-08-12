87e383f4889a645d8e3e73bbba5e916d93c840d3
surekha-degala <surekhadegala22@gmail.com>
2025-08-12 17:15:06 +0530

    Create symmetric tree.java

---
+++ Trees/symmetric tree.java
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+…        if (n1 == null && n2 == null) return true;
+        if (n1 == null || n2 == null) return false;
+        return n1.val == n2.val && dfs(n1.left, n2.right) && dfs(n1.right , n2.left);
+    }
+}
