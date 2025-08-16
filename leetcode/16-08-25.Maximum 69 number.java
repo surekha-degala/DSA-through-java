60beeab6b6eb77a3fb3be497d765a23a67d5c248
surekha-degala <surekhadegala22@gmail.com>
2025-08-16 23:41:58 +0530

    Update 16-08-25.Maximum 69 number.java

--- leetcode/16-08-25.Maximum 69 number.java
+++ leetcode/16-08-25.Maximum 69 number.java
@@ -11,3 +11,20 @@ class Solution {
         return result;
     }
 }
+//num method
+int num = 9669;
+
+// find largest power of 10 <= num
+int div = 1;
+while (num / div >= 10) div *= 10;   // here div becomes 1000
+
+while (div > 0) {
+    int digit = (num / div) % 10;    // pick current left-most digit
+    if (digit == 6) {
+        num += 3 * div;              // flip 6 -> 9 adds 3 * place value
+        break;
+    }
+    div /= 10;                        // move right one digit
+}
+
+// num is now 9969
